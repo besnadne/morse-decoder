0ba0e71527a3866114edb9810d8f58803399dfd6
besnadne <serpov.box@gmail.com>
2022-01-28 21:59:58 +0200

    init: start working with task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    expr = expr.split('');
+    
+    const map = {
+        '-': 10,
+        '.': 11,
+        ' ': '*'
+    }
+    for (let key in map) {
+        expr.forEach(element => {
+            if(element === key){
+                arr.push(map[key])
+            }
+        });
+    }
+    arr = arr.join('').split('')
+    while (arr.length < 10){
+        if(arr[1] === '*'){
+            arr.unshift(0+'')
+        }else{
+            arr.unshift('*')
+        } 
+    } 
+  
+   return arr.join('')
 }
 
 module.exports = {
